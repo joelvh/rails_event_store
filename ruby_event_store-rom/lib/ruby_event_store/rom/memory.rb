38f26220be3adbd21dc4096ee36dc7df9eab681e
Joel Van Horn <joel@joelvanhorn.com>
2018-10-13 22:07:17 -0400

    Pass event_id to exception

--- ruby_event_store-rom/lib/ruby_event_store/rom/memory.rb
+++ ruby_event_store-rom/lib/ruby_event_store/rom/memory.rb
@@ -25,7 +25,7 @@ module RubyEventStore
           env.register_error_handler :unique_violation, -> ex {
             case ex
             when TupleUniquenessError
-              raise EventDuplicatedInStream if ex.message =~ /event_id/
+              raise EventDuplicatedInStream.new(ex.event_id) if ex.event_id
               raise WrongExpectedEventVersion
             end
           }
--- ruby_event_store-rom/lib/ruby_event_store/rom/tuple_uniqueness_error.rb
+++ ruby_event_store-rom/lib/ruby_event_store/rom/tuple_uniqueness_error.rb
@@ -3,16 +3,37 @@ module RubyEventStore
     class TupleUniquenessError < StandardError
       class << self
         def for_event_id(event_id)
-          new "Uniquness violated for event_id (#{event_id.inspect})"
+          new(
+            "Uniquness violated for event_id (#{event_id.inspect})",
+            event_id: event_id
+          )
         end
 
         def for_stream_and_event_id(stream_name, event_id)
-          new "Uniquness violated for stream (#{stream_name.inspect}) and event_id (#{event_id.inspect})"
+          new(
+            "Uniquness violated for stream (#{stream_name.inspect}) and event_id (#{event_id.inspect})",
+            event_id: event_id,
+            stream_name: stream_name
+          )
         end
 
         def for_stream_and_position(stream_name, position)
-          new "Uniquness violated for stream (#{stream_name.inspect}) and position (#{position.inspect})"
+          new(
+            "Uniquness violated for stream (#{stream_name.inspect}) and position (#{position.inspect})",
+            stream_name: stream_name,
+            position: position
+          )
         end
+      end
+      
+      attr_reader :event_id, :stream_name, :position
+      
+      def initialize(message, event_id: nil, stream_name: nil, position: nil)
+        @event_id    = event_id
+        @stream_name = stream_name
+        @position    = position
+
+        super(message)
       end
     end
   end
